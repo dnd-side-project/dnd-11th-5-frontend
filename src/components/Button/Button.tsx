0ae0165690f67f80cd8ecfff75608c2aec3b59e1
Jeongmin Lee <clcl6084@gmail.com>
2024-07-10 08:54:21 +0900

    ✨ feat: 스토리북 초기 세팅

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import { Button as SButton } from "@radix-ui/themes";
+import React from "react";
+
+const Button = () => {
+  return <SButton className="bg-blue-30">Test</SButton>;
+};
+
+export default Button;
