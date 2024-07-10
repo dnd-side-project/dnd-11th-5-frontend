d8c15b70cad1218e1711db9e4b2ee728f0161d9d
Jeongmin Lee <clcl6084@gmail.com>
2024-07-10 09:07:25 +0900

    💅 style: 코드 전체 포매팅

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import { Button as SButton } from "@radix-ui/themes";
 import React from "react";
 
 const Button = () => {
-  return <SButton className="bg-blue-30">Test</SButton>;
+  return <SButton className="">Test</SButton>;
 };
 
 export default Button;
