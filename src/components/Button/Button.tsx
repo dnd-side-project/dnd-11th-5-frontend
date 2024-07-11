351d27a9d2ece22a34e7b5d3cd93380db5b81146
Jeongmin Lee <clcl6084@gmail.com>
2024-07-12 00:21:27 +0900

    🔨 settings: remove storybook #1

--- src/components/Button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { Button as SButton } from "@radix-ui/themes";
-import React from "react";
-
-const Button = () => {
-  return <SButton className="">Test</SButton>;
-};
-
-export default Button;
