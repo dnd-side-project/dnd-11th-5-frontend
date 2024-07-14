6a290ea41a4b1b38c672fac31bd9fa2f3e18a1f4
Jeongmin Lee <clcl6084@gmail.com>
2024-07-15 02:48:33 +0900

    🔨 settings: 빌드 시점에 환경 변수의 유효성을 검사를 위한 jiti 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+// https://env.t3.gg/docs/nextjs#validate-schema-on-build-(recommended)
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./src/env");
+
 const nextConfig = {};
 
 export default nextConfig;
