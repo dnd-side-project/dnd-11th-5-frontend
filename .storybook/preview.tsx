36bee0b756c712d307d8fcbe30e1196a0020636c
Jeongmin Lee <clcl6084@gmail.com>
2024-08-01 03:29:22 +0900

    💅 style: 폰트 전체 적용 #8

---
+++ .storybook/preview.tsx
@@ -0,0 +1,28 @@
+import type { Preview } from "@storybook/react";
+import React from "react";
+
+import "../src/styles/globals.css";
+import "../src/styles/theme.css";
+import { cn } from "../src/utils/cn";
+import { Pretendard } from "../src/app/layout";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    decorators: [
+      (Story) => (
+        // TODO: 스토리북 전체에 폰트적용이 안됨. 방법을 찾아야함
+        <div className={cn(Pretendard.variable, "font-pretendard")}>
+          <Story />
+        </div>
+      ),
+    ],
+  },
+};
+
+export default preview;
