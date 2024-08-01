333d2cdf00d925593b09ab48678a88fc8417b65b
Jeongmin Lee <clcl6084@gmail.com>
2024-08-02 01:10:53 +0900

    💅 style: svg 파일 fill="currentColor" 설정 및 re-export 파일 생성 #13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { CAMERA_ICON } from "@/components/icons";
+
 export default function Home() {
   return (
-    <main className="flex flex-wrap gap-4 bg-indigo-300 text-2xl">
+    <main className="text-2xl flex flex-wrap gap-4 bg-indigo-300">
       <div className="h-[100px] w-full bg-red-200"></div>
+      <div>
+        <CAMERA_ICON
+          className="text-2xl text-green-500"
+          width={50}
+          height={50}
+        />
+      </div>
       <div className="h-[100px] w-full bg-red-200"></div>
       <div className="h-[100px] w-full bg-red-200"></div>
       <div className="h-[100px] w-full bg-red-200"></div>
