504030ced99562b64024f7468f5f930cb17af0a1
Jeongmin Lee <clcl6084@gmail.com>
2024-08-03 22:55:50 +0900

    ✨ feat: chip 컴포넌트 구현 및 스토리북 추가 #15

---
+++ src/components/common/Chip/ReviewChip/ReviewChip.tsx
@@ -0,0 +1,30 @@
+import { FC, HtmlHTMLAttributes } from "react";
+
+import { cn } from "@/utils/cn";
+
+export interface Props
+  extends Omit<HtmlHTMLAttributes<HTMLButtonElement>, "children"> {
+  active?: boolean;
+  label: string;
+}
+
+const ReviewChip: FC<Props> = ({ active = false, label, ...props }) => {
+  return (
+    <button
+      type="button"
+      className={cn(
+        "w-auto h-auto duration-300 rounded-[17px] py-[12px] px-[16px] border-[1px] ",
+        "flex flex-col items-center justify-center",
+        active
+          ? "bg-primary-05 border-primary-01 text-gray-scale-700"
+          : "bg-gray-scale-0 border-gray-scale-100 text-gray-scale-500",
+        props.className,
+      )}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default ReviewChip;
