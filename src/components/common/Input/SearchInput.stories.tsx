0442352005ac961d135c47119229898f653d2d69
Jeongmin Lee <clcl6084@gmail.com>
2024-08-03 23:09:50 +0900

    ✨ feat: input 컴포넌트 구현 및 스토리북 추가 #15

---
+++ src/components/common/Input/SearchInput.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import SearchInput from "./SearchInput";
+
+const meta: Meta<typeof SearchInput> = {
+  title: "Input/SearchInput",
+  component: SearchInput,
+  parameters: {
+    layout: "centered",
+  },
+
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const SearchHistory: Story = {
+  args: {},
+};
