f228f9d8d0be175b220570c9deb21997a9d6ca95
Jeongmin Lee <clcl6084@gmail.com>
2024-08-04 18:58:27 +0900

    ✨ feat: 바텀 네비게이션바 구현 및 스토리북 추가 #15

---
+++ src/layout/Mobile/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,60 @@
+import * as NM from "@radix-ui/react-navigation-menu";
+
+import {
+  CalendarCheckIcon,
+  ChatBubbleDotsIcon,
+  HomeIcon,
+  PinLocationIcon,
+  UserIcon,
+} from "@/components/icons";
+import { cn } from "@/utils/cn";
+
+import NavigationBarButton from "./NavigationBarButton";
+
+const navigations = [
+  { name: "홈", slug: "/", icon: <HomeIcon width={20} height={20} /> },
+  {
+    name: "캘린더",
+    slug: "/calendar",
+    icon: <CalendarCheckIcon width={20} height={20} />,
+  },
+  {
+    name: "지역",
+    slug: "/map",
+    icon: <PinLocationIcon width={20} height={20} />,
+  },
+  {
+    name: "채팅",
+    slug: "/chat",
+    icon: <ChatBubbleDotsIcon width={20} height={20} />,
+  },
+  { name: "MY", slug: "/mypage", icon: <UserIcon width={20} height={20} /> },
+];
+
+const NavigationBar = () => {
+  return (
+    <NM.Root
+      className={cn(
+        "w-full h-[60px] px-[26px] py-[12px] rounded-t-[24px] max-w-none lg:max-w-[450px]",
+        "fixed bottom-0",
+        "bg-gray-scale-0",
+      )}
+    >
+      <NM.List className="flex  h-full w-full  items-center justify-between ">
+        {navigations.map((nav) => (
+          <NM.Item
+            key={nav.name}
+            className="flex w-[36px] items-center justify-center text-caption1-medium-nav"
+          >
+            <NavigationBarButton href={nav.slug}>
+              {nav.icon}
+              {nav.name}
+            </NavigationBarButton>
+          </NM.Item>
+        ))}
+      </NM.List>
+    </NM.Root>
+  );
+};
+
+export default NavigationBar;
