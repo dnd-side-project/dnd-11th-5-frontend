ef75fa60ca3f8e206eb28459de979e040c3f526b
Jeongmin Lee <clcl6084@gmail.com>
2024-08-07 01:25:54 +0900

    💅 style: 페스티발 카드 컴포넌트 스크랩 버튼 추가 #15

--- src/components/core/Card/TrendFestivalCard/TrendFestivalCard.stories.tsx
+++ src/components/core/Card/TrendFestivalCard/TrendFestivalCard.stories.tsx
@@ -40,3 +40,41 @@ export const Default: Story = {
     ),
   ],
 };
+
+export const RegionCard: Story = {
+  args: {
+    festival: {
+      image: "/images/festivalTrend.png",
+      title: "한강 페스티벌",
+      location: "서울 광진구",
+      duration: "8월 1일 ~ 8월 4일",
+      dday: "D-2",
+    },
+    isScrabed: false,
+    onScrab: () => {},
+  },
+  render: (args) => (
+    <div className="w-[200px]">
+      <TrendFestivalCard {...args} />
+    </div>
+  ),
+};
+
+export const RegionCardScrabed: Story = {
+  args: {
+    festival: {
+      image: "/images/festivalTrend.png",
+      title: "한강 페스티벌",
+      location: "서울 광진구",
+      duration: "8월 1일 ~ 8월 4일",
+      dday: "D-2",
+    },
+    isScrabed: true,
+    onScrab: () => {},
+  },
+  render: (args) => (
+    <div className="w-[200px]">
+      <TrendFestivalCard {...args} />
+    </div>
+  ),
+};
--- src/components/core/Card/TrendFestivalCard/TrendFestivalCard.tsx
+++ src/components/core/Card/TrendFestivalCard/TrendFestivalCard.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { FC, HTMLAttributes } from "react";
 
+import { ScrabIcon } from "@/components/icons";
 import { cn } from "@/utils/cn";
 
+import IconButton from "../../Button/IconButton/IconButton";
 import DateTag from "../../Tag/DateTag/DateTag";
 
 type FestivalModel = {
@@ -15,9 +17,18 @@ type FestivalModel = {
 
 interface Props extends Omit<HTMLAttributes<HTMLButtonElement>, "type"> {
   festival: FestivalModel;
+  isScrabed?: boolean;
+  onScrab?: () => void;
+  onClick: () => void;
 }
 
-const TrendFestivalCard: FC<Props> = ({ festival, ...props }) => {
+const TrendFestivalCard: FC<Props> = ({
+  festival,
+  isScrabed,
+  onClick,
+  onScrab,
+  ...props
+}) => {
   return (
     <button
       type="button"
@@ -25,6 +36,7 @@ const TrendFestivalCard: FC<Props> = ({ festival, ...props }) => {
         "w-full h-auto flex flex-col gap-[12px] rounded-t-[8px] ",
         props.className,
       )}
+      onClick={onClick}
       {...props}
     >
       <div className="relative h-[136px] w-full rounded-[8px]">
@@ -33,6 +45,22 @@ const TrendFestivalCard: FC<Props> = ({ festival, ...props }) => {
           label={festival.dday}
           className="absolute left-[14px] top-[12px]"
         />
+        {!!onScrab && (
+          <IconButton
+            type="button"
+            className="absolute right-[14px] top-[12px] flex size-[22px] items-center justify-center rounded-full bg-gray-scale-700"
+            onClick={onScrab}
+            icon={
+              <ScrabIcon
+                width={14}
+                height={14}
+                className={cn(
+                  isScrabed ? "text-primary-01" : "text-gray-scale-0",
+                )}
+              />
+            }
+          />
+        )}
       </div>
 
       <div className="flex w-full flex-col items-start">
