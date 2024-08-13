3697a9502de5406b05a6edcb1de5a82cbbf5d9fd
Jeongmin Lee <clcl6084@gmail.com>
2024-08-14 02:34:22 +0900

    💅 style: 타입 변경에의한 컴포넌트 수정 #6

--- src/components/core/Button/SquareTabButton/SquareTabButton.tsx
+++ src/components/core/Button/SquareTabButton/SquareTabButton.tsx
@@ -4,15 +4,21 @@ import { cn } from "@/utils/cn";
 
 export interface Props
   extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "children"> {
+  emoji: string;
   active: boolean;
   label: string;
 }
 
-const SquareTabButton: FC<Props> = ({ label, active = false, ...props }) => {
+const SquareTabButton: FC<Props> = ({
+  label,
+  active = false,
+  emoji,
+  ...props
+}) => {
   return (
     <button
       className={cn(
-        "size-[108px] duration-300 rounded-[8px]",
+        "size-[108px] duration-300 rounded-[8px] py-[20px]",
         "flex flex-col items-center justify-center gap-[12px] mx-auto",
         active
           ? "bg-primary-05 border-[1px] border-primary-01"
@@ -23,10 +29,12 @@ const SquareTabButton: FC<Props> = ({ label, active = false, ...props }) => {
     >
       <div
         className={cn(
-          "w-[36px] h-[36px] rounded-full duration mx-auto",
-          active ? "bg-primary-03" : "bg-gray-scale-200",
+          "rounded-full duration mx-auto",
+          "flex justify-center items-center text-[28px]",
         )}
-      />
+      >
+        {emoji ?? "🎊"}
+      </div>
       <span className="max-w-[90px] truncate text-button2-medium text-gray-scale-700">
         {label}
       </span>
--- src/components/core/List/BasicTitle/BasicTile.tsx
+++ src/components/core/List/BasicTitle/BasicTile.tsx
@@ -3,12 +3,16 @@ import { ButtonHTMLAttributes, FC } from "react";
 import { cn } from "@/utils/cn";
 
 export interface Props
-  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "children"> {
+  extends Omit<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    "children" | "className"
+  > {
   active: boolean;
   label: string;
+  className?: string;
 }
 
-const BasicTile: FC<Props> = ({ active, label, ...props }) => {
+const BasicTile: FC<Props> = ({ active, label, className, ...props }) => {
   return (
     <button
       type="button"
@@ -18,11 +22,16 @@ const BasicTile: FC<Props> = ({ active, label, ...props }) => {
         active
           ? "border-[1px] border-primary-01 bg-primary-05"
           : "bg-gray-scale-50 text-gray-scale-600",
-        props.className,
+        className,
       )}
       {...props}
     >
-      <span className="w-full text-left text-caption2-medium text-gray-scale-600">
+      <span
+        className={cn(
+          "w-full text-left  text-gray-scale-600",
+          "text-button2-medium whitespace-nowrap",
+        )}
+      >
         {label}
       </span>
     </button>
