80d89b5878bc1dfeac2eaa89fb6c2218c5b5a8b5
Jeongmin Lee <clcl6084@gmail.com>
2024-08-16 01:46:04 +0900

    💅 style: 홈헤더 스타일 수정

--- src/layout/Mobile/MobileHeader/HomeHeader/HomeHeader.tsx
+++ src/layout/Mobile/MobileHeader/HomeHeader/HomeHeader.tsx
@@ -1,25 +1,27 @@
-"use client";
-
+import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 
-import { AlarmIcon, CameraIcon, SearchIcon } from "@/components/icons";
+import { SearchIcon } from "@/components/icons";
 
 interface Props {}
 
 const HomeHeader: FC<Props> = ({}) => {
   return (
-    <header className="fixed top-0 flex h-[44px] w-full max-w-none items-center justify-between bg-gray-scale-0 px-[10px] text-gray-900 lg:max-w-[450px]">
-      <button type="button" onClick={() => {}}>
-        <CameraIcon width={24} height={24} />
-      </button>
+    <header className="fixed top-0 flex h-[44px] w-full max-w-none items-center justify-between bg-gray-scale-0 px-[16px] text-gray-900 lg:max-w-[450px]">
+      <Link href="/">
+        <Image
+          width={84}
+          height={30}
+          src="/images/fiestaLogo.png"
+          alt="fiesta-logo"
+        />
+      </Link>
 
       <div className="flex items-center justify-center gap-[12px]">
-        <button type="button" onClick={() => {}}>
+        <Link href="/search">
           <SearchIcon width={24} height={24} className="text-gray-scale-900" />
-        </button>
-        <button type="button" onClick={() => {}}>
-          <AlarmIcon width={24} height={24} className="text-gray-scale-0" />
-        </button>
+        </Link>
       </div>
     </header>
   );
