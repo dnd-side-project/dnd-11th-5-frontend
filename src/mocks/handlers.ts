113e2edbf5074841d60799aeecd27a099074a248
Jeongmin Lee <clcl6084@gmail.com>
2024-08-18 04:53:52 +0900

    ♻️ refactor: 피드백 수용해서 핸들러 리팩토링 및 faker-js 적용

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,33 +1,3 @@
-import { http, HttpResponse } from "msw";
+import { FestivalHandler } from "./data/festivalMocks/hotFestivalHandler";
 
-import HotFestival from "./data/festivalMocks/HotFestival.json";
-import HotFestivalPage1 from "./data/festivalMocks/HotFestival_1.json";
-import HotFestivalPage2 from "./data/festivalMocks/HotFestival_2.json";
-import HotFestivalPage3 from "./data/festivalMocks/HotFestival_3.json";
-
-export const handlers = [
-  http.get(
-    "https://odiga.shop/api/v1/festivals/mostlike",
-
-    ({ request }) => {
-      const url = new URL(request.url);
-      const page = url.searchParams.get("page");
-
-      if (!!page) {
-        const pageNumber = parseInt(page);
-        const pageIndex = pageNumber % 3;
-        if (pageIndex === 1) {
-          return HttpResponse.json(HotFestivalPage1);
-        }
-        if (pageIndex === 2) {
-          return HttpResponse.json(HotFestivalPage2);
-        }
-        if (pageIndex === 0) {
-          return HttpResponse.json(HotFestivalPage3);
-        }
-      }
-
-      return HttpResponse.json(HotFestival);
-    },
-  ),
-];
+export const handlers = [...FestivalHandler];
