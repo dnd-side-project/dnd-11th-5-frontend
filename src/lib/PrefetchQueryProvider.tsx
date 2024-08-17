873b61ffbdf458fed5ec87bd185881efb9d70651
Jeongmin Lee <clcl6084@gmail.com>
2024-08-18 04:17:19 +0900

    🔨 settings: 리액트쿼리 defaultOption과 prefetch 래퍼 컴포넌트 생성 #26

---
+++ src/lib/PrefetchQueryProvider.tsx
@@ -0,0 +1,33 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+  QueryFunction,
+  QueryKey,
+} from "@tanstack/react-query";
+import React from "react";
+
+interface PrefetchQueryProviderProps {
+  queryKey: QueryKey;
+  queryFn: QueryFunction;
+  children: React.ReactNode;
+}
+
+const PrefetchQueryProvider = async ({
+  queryKey,
+  queryFn,
+  children,
+}: PrefetchQueryProviderProps) => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey,
+    queryFn,
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {children}
+    </HydrationBoundary>
+  );
+};
+
+export default PrefetchQueryProvider;
--- src/lib/ReactQueryProvider.tsx
+++ src/lib/ReactQueryProvider.tsx
@@ -6,7 +6,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren } from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function ReactQueryProvider({ children }: PropsWithChildren) {
   return (
