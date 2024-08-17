d4d4453416126a5ec97048ed67d2a62cb5c57257
Jeongmin Lee <clcl6084@gmail.com>
2024-08-17 19:49:01 +0900

    ♻️ refactor: 쿼리 클리이언트 인스턴스 생성시 useState 사용안하게끔 수정 #26

--- src/lib/ReactQueryProvider.tsx
+++ src/lib/ReactQueryProvider.tsx
@@ -4,13 +4,13 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient();
 
 function ReactQueryProvider({ children }: PropsWithChildren) {
-  const [client] = useState(new QueryClient());
-
   return (
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
