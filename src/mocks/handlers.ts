556bb6b4219db6c7e29de7d5eec30ce1ef6e472c
Jeongmin Lee <clcl6084@gmail.com>
2024-08-18 07:29:11 +0900

    ✅ test: 이번주 페스티벌에 대한 목데이터 핸들러 추가 #30

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,4 @@
 import { FestivalHandler } from "./data/festivalMocks/hotFestivalHandler";
+import { thisWeekFestivalHandler } from "./data/festivalMocks/thisWeekFestivalHandler";
 
-export const handlers = [...FestivalHandler];
+export const handlers = [...FestivalHandler, ...thisWeekFestivalHandler];
