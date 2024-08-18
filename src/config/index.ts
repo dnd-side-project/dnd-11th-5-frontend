7c6cd4b1c30c7352d43ff23e2a3d975cf529f78d
Jeongmin Lee <clcl6084@gmail.com>
2024-08-18 23:47:11 +0900

    🔧 chore: 온보딩 관련 값들 상수화 #6

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export { default as FIESTA_ENDPOINTS } from "./apiEndpoints";
+export { default as ONBOARDING_SETTING } from "./onBoardingSetting";
 export { default as REVALIDATE_DURATION } from "./revalidateDuration";
 export { default as VALIDATION_ERROR_MESSAGES } from "./validationError";
