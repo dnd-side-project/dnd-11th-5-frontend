2dd7d88a44cf8098416f4c925954f46528810fb1
Jeongmin Lee <clcl6084@gmail.com>
2024-08-25 00:20:02 +0900

    ✨ feat: 실시간 핫한 후기 API, Type 선언 #31

---
+++ src/apis/review/topReviews/topReviewsType.ts
@@ -0,0 +1,15 @@
+export interface TopReview {
+  reviewId: number;
+  festivalId: number;
+  festivalName: string;
+  content: string;
+  rating: number;
+  images: {
+    imageId: number;
+    imageUrl: string;
+  };
+  keywords: {
+    keywordId: number;
+    keyword: string;
+  }[];
+}
