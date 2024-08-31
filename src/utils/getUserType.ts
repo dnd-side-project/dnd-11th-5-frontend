fcdd2b78427402ef03d1ff8167605ab45e7b3006
Jeongmin Lee <clcl6084@gmail.com>
2024-08-31 22:27:52 +0900

    ✨ feat: 유저타입에 따른 동적 클래스네임, 이미지를 위한 객체 생성 #34

---
+++ src/utils/getUserType.ts
@@ -0,0 +1,14 @@
+export const UserTypeText: Record<string, string> = {
+  1: "text-secondary-pink-text",
+  2: "text-secondary-orange-text",
+  3: "text-secondary-yellow-text",
+  4: "text-secondary-green-text",
+  5: "text-secondary-blue-text",
+};
+export const UserTypeImage: Record<string, string> = {
+  1: "/images/romantist.png",
+  2: "/images/partypeople.png",
+  3: "/images/inspirer.png",
+  4: "/images/healer.png",
+  5: "/images/explorer.png",
+};
