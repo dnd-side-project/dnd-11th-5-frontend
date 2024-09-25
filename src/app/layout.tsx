adf3f7ab8c58628123886c1222e5ede9edf9c2f1
Jeongmin Lee <clcl6084@gmail.com>
2024-09-26 01:02:05 +0900

    🔧 chore: 카카오맵 스크립트 추가 #36

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import "../styles/globals.css";
 import "../styles/reset.css";
 
 import type { Metadata } from "next";
+import Script from "next/script";
 import { ReactNode } from "react";
 
+import { env } from "@/env";
 import MobileLayout from "@/layout/Mobile/MobileLayout";
 import { MSWProvider } from "@/lib/MSWProvider";
 import ReactQueryProvider from "@/lib/ReactQueryProvider";
@@ -26,7 +28,13 @@ export default function RootLayout({
       <body className={cn(Pretendard.className)} suppressHydrationWarning>
         <MSWProvider>
           <ReactQueryProvider>
-            <MobileLayout>{children}</MobileLayout>
+            <MobileLayout>
+              <Script
+                src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`}
+                strategy="beforeInteractive"
+              />
+              {children}
+            </MobileLayout>
           </ReactQueryProvider>
         </MSWProvider>
       </body>
