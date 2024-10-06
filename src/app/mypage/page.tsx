397798ffd2b50a43bb75e3da6bdd8cbbe0e5d6c1
Jeongmin Lee <clcl6084@gmail.com>
2024-10-06 23:21:15 +0900

    ✨ feat: mypage page,view 퍼블리싱 #50

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,10 +1,12 @@
 import NavigationBar from "@/layout/Mobile/NavigationBar";
 
+import MypageHeader from "./_components/MypageHeader";
 import MyPageView from "./view";
 
 export default function Map() {
   return (
     <div className="mb-[60px] ">
+      <MypageHeader />
       <MyPageView />
       <NavigationBar />
     </div>
