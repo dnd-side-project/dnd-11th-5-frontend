315be43ba13c0328600cd32b06e5a5165e803cc3
Jeongmin Lee <clcl6084@gmail.com>
2024-10-06 20:22:58 +0900

    🐛 fix: middleware에 auth 바인딩 롤백 및 라우트 베이스 핸들링 추가 #45

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,41 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { match } from "path-to-regexp";
 
-import { auth } from "@/auth";
+import { getServerSideSession } from "./apis/auth/auth";
 
 const matchersForAuth = ["/mypage"];
-const matchersForSignIn = ["/sign-in"];
+const matchersForSignIn = ["/auth/sign-in"];
 export async function middleware(request: NextRequest) {
-  // 인증이 필요한 페이지 접근 제어!
+  // * 인증이 필요한 페이지 접근 제어!
   if (isMatch(request.nextUrl.pathname, matchersForAuth)) {
-    return (await auth()) // 세션 정보 확인
+    const session = await getServerSideSession();
+
+    if (!!session && !session.isProfileRegistered) {
+      return NextResponse.redirect(new URL("/onboarding", request.url));
+    }
+
+    return (await getServerSideSession()) // 세션 정보 확인
       ? NextResponse.next()
-      : NextResponse.redirect(new URL("/auth/sign-in", request.url));
-    // : NextResponse.redirect(new URL(`/signin?callbackUrl=${request.url}`, request.url))
+      : NextResponse.redirect(
+          new URL(`/auth/sign-in?callbackUrl=${request.url}`, request.url),
+        );
   }
-  // 인증 후 회원가입 및 로그인 접근 제어!
-  if (isMatch(request.nextUrl.pathname, matchersForSignIn)) {
-    return (await auth())
+  // * 인증 후 회원가입 및 로그인 접근 제어!
+  if (matchersForSignIn.includes(request.nextUrl.pathname)) {
+    const session = await getServerSideSession();
+
+    if (!!session && !session?.isProfileRegistered) {
+      return NextResponse.redirect(new URL("/onboarding", request.url));
+    }
+
+    !!session
       ? NextResponse.redirect(new URL("/", request.url))
       : NextResponse.next();
   }
   return NextResponse.next();
 }
 
-// 경로 일치 확인!
+// * 경로 일치 확인!
 function isMatch(pathname: string, urls: string[]) {
   return urls.some((url) => !!match(url)(pathname));
 }
