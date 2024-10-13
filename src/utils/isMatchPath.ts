cc825b3f0d59bc5186116bea5e944245b5cebe01
Jeongmin Lee <clcl6084@gmail.com>
2024-10-13 18:02:06 +0900

    ♻️ refactor: path-to-regexp 패키지 삭제 #70

--- src/utils/isMatchPath.ts
+++ src/utils/isMatchPath.ts
@@ -1,4 +1,2 @@
-import { match } from "path-to-regexp";
-
 export const isMatchPath = (pathname: string, urls: string[]) =>
-  urls.some((url) => !!match(url)(pathname));
+  urls.some((url) => pathname.includes(url));
